98bf1bef0df83eaa1f73e21eff2b2030e57c0a3e
Kevin Zong <7kevinzong@gmail.com>
2023-12-07 15:38:42 -0500

    feat(intro): add headshot and intro blurb

---
+++ components/intro.tsx
@@ -0,0 +1,59 @@
+"use client";
+
+import { motion } from "framer-motion";
+import Image from "next/image";
+import React from "react";
+
+export default function Intro() {
+  return (
+    <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
+      <div className="flex items-center justify-center">
+        <div className="relative">
+          <motion.div
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "tween",
+              duration: 0.2,
+            }}
+          >
+            <Image
+              alt="Kevin Zong portrait"
+              src="/headshot.png"
+              width="192"
+              height="192"
+              quality="95"
+              priority={true}
+              className="h-24 w-24 rounded-full object-cover border-[0.35rem]
+            border-white shadow-xl"
+            />
+          </motion.div>
+          <motion.span
+            className="absolute bottom-0 right-0 text-3xl"
+            initial={{ opacity: 0, scale: 0 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+              type: "spring",
+              stiffness: 125,
+              delay: 0.1,
+              duration: 0.7,
+            }}
+          >
+            👋
+          </motion.span>
+        </div>
+      </div>
+      <motion.p
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5]
+        sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Kevin Zong.</span> I'm a{" "}
+        <span className="font-bold">backend developer</span> with{" "}
+        <span className="font-bold">2 years of experience</span>. I enjoy
+        building <span className="italic">websites & scalable cloud apps</span>.
+      </motion.p>
+    </section>
+  );
+}
