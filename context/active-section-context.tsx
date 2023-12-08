5a3fc0e65f99c018799333d97ddc8dcf30d1d9f1
Kevin Zong <7kevinzong@gmail.com>
2023-12-08 16:42:50 -0500

    feat: prevent intermediary sections from being set to active when jumping to a section from the navbar

--- context/active-section-context.tsx
+++ context/active-section-context.tsx
@@ -10,6 +10,8 @@ type ActiveSectionContextProviderProps = {
 type ActiveSectionContextType = {
   activeSection: SectionName;
   setActiveSection: React.Dispatch<React.SetStateAction<SectionName>>;
+  timeOfLastClick: number;
+  setTimeOfLastClick: React.Dispatch<React.SetStateAction<number>>;
 };
 
 export const ActiveSectionContext =
@@ -19,12 +21,15 @@ export default function ActionSectionContextProvider({
   children,
 }: ActiveSectionContextProviderProps) {
   const [activeSection, setActiveSection] = useState<SectionName>("Home");
+  const [timeOfLastClick, setTimeOfLastClick] = useState(0); // need to keep track of this to prevent activeSection from changing when user clicks on a link
 
   return (
     <ActiveSectionContext.Provider
       value={{
         activeSection,
         setActiveSection,
+        timeOfLastClick,
+        setTimeOfLastClick,
       }}
     >
       {children}
