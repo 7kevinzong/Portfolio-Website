0ac258d27a6e63579c32f73f9269322875c6593b
Kevin Zong <7kevinzong@gmail.com>
2023-12-17 18:35:29 -0500

    feat: add vercel analytics tracking

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,6 +7,7 @@ import { Toaster } from "react-hot-toast";
 import Footer from "@/components/footer";
 import ThemeSwitch from "@/components/theme-switch";
 import ThemeContextProvider from "@/context/theme-context";
+import { Analytics } from "@vercel/analytics/react";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -45,6 +46,7 @@ export default function RootLayout({
             <Toaster position="top-right" />
             <Footer />
             <ThemeSwitch />
+            <Analytics />
           </ActionSectionContextProvider>
         </ThemeContextProvider>
       </body>
